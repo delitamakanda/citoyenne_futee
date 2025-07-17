0a5d75f5e5e4f5098b86982b5f99712683ff3b56
Délita Makanda <delita.makanda@gmail.com>
2025-07-18 01:38:37 +0200

    fix migrations

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
 
 class CustomUser(AbstractUser):
     ROLES = [
@@ -10,3 +11,19 @@ class CustomUser(AbstractUser):
     ]
     role = models.CharField(max_length=10, choices=ROLES, default='student')
     avatar = models.CharField(max_length=255, blank=True)
+    groups = models.ManyToManyField(
+        Group,
+        blank=True,
+        verbose_name='groups',
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name="customuser_set",
+        related_query_name="customuser",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        blank=True,
+        verbose_name='user permissions',
+        help_text='Specific permissions for this user.',
+        related_name="customuser_set",
+        related_query_name="customuser",
+    )
